6e5dc155e2a517b3b1c0d9b64a3ea8979c3fbe55
Greethika-D <140243827+Greethika-D@users.noreply.github.com>
2023-07-28 23:41:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,23 @@
-import tkinter as tk
-import customtkinter as ctk 
+import requests
+import io
+import os
+from PIL import Image
+from config import key
+import streamlit as st
 
-from PIL import ImageTk
-from authtoken import auth_token
+st.title("Image generation")
+prompt = st.text_input("Enter your prompt here")
+if st.button("Generate Image"):
+  with st.spinner("Processing"):
+    r = requests.post('https://clipdrop-api.co/text-to-image/v1',
+      files = {
+          'prompt': (None, prompt, 'text/plain')
+      },
+      headers = { 'x-api-key': key }
+    )
 
-import torch
-from torch import autocast
-from diffusers import StableDiffusionPipeline 
-
-# Create the app
-app = tk.Tk()
-app.geometry("532x632")
-app.title("Stable Bud") 
-ctk.set_appearance_mode("dark") 
-
-prompt = ctk.CTkEntry(height=40, width=512, text_font=("Arial", 20), text_color="black", fg_color="white") 
-prompt.place(x=10, y=10)
-
-lmain = ctk.CTkLabel(height=512, width=512)
-lmain.place(x=10, y=110)
-
-modelid = "CompVis/stable-diffusion-v1-4"
-device = "cuda"
-pipe = StableDiffusionPipeline.from_pretrained(modelid, revision="fp16", torch_dtype=torch.float16, use_auth_token=auth_token) 
-pipe.to(device) 
-
-def generate(): 
-    with autocast(device): 
-        image = pipe(prompt.get(), guidance_scale=8.5)["sample"][0]
-    
-    image.save('generatedimage.png')
-    img = ImageTk.PhotoImage(image)
-    lmain.configure(image=img) 
-
-trigger = ctk.CTkButton(height=40, width=120, text_font=("Arial", 20), text_color="white", fg_color="blue", command=generate) 
-trigger.configure(text="Generate") 
-trigger.place(x=206, y=60) 
-
-app.mainloop()  
+    if (r.ok):
+        images = Image.open(io.BytesIO(r.content))
+        st.image(image=images)
+    else:
+      r.raise_for_status()
